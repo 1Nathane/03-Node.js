b028861f90514d477c77325da849c0b01a6e3598
1Nathane <nathane.miranda@gmail.com>
2021-04-11 16:45:31 -0300

    teste

--- PrimeiroProjeto/app.js
+++ PrimeiroProjeto/app.js
@@ -2,3 +2,8 @@ const count = 5;
 console.log('count: %d', count); // imprime count: 5
 console.log('count:', count);
 console.log('count: ${count}');
+console.log('count: ${count}');
+console.log('count: ${count}');
+console.log('count: ${count}');
+//teste ...
+
