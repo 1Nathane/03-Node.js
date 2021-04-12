aa73378a88aec23abb78876543bdaf028f23a18c
1Nathane <nathane.miranda@gmail.com>
2021-04-11 21:56:36 -0300

    Desafio 3

--- TODO-LIST/Desafio03_modulo.js
+++ TODO-LIST/Desafio03_modulo.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
+var moment = require("moment")
 
 const arquivo = 'log.txt'
 
 const funcaoImprimirLog = (conteudo) =>{
     
-    const data = new Date()
+    /*const data = new Date()
     const dia = data.getDate()
     const mes = data.getMonth() + 1
     const ano = data.getFullYear()
@@ -14,7 +15,9 @@ const funcaoImprimirLog = (conteudo) =>{
 
     const dataHora = dia + '/' + mes + '/' + ano + ' ' + hora + ':' + minutos + ':' + segundos
     
-    textoLog =  dataHora + ' -> ' + conteudo + '\n'
+    textoLog =  dataHora + ' -> ' + conteudo + '\n'*/
+
+    const textoLog = moment().format("DD/MM/YYYY HH:mm:ss") + ' -> ' + conteudo + '\n'
     fs.appendFileSync(arquivo, textoLog)
 } 
 
