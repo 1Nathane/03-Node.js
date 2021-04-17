f9db8921b43f709d558f69fb9f3a082be644146a
1Nathane <nathane.miranda@gmail.com>
2021-04-17 18:22:21 -0300

    Aula

--- TODO-LIST/app.js
+++ TODO-LIST/app.js
@@ -1,11 +1,16 @@
-const funcaoTesteIncremento = require('./modulo')
-//const sobrenome = require('./modulo');
+// const funcaoTesteIncremento = require('./modulo')
+// //const sobrenome = require('./modulo');
 
-const nome = 'Nathane'
+// const nome = 'Nathane'
 
-//console.log('Estou em -> app.js')
-//console.log(nome, sobrenome);
+// //console.log('Estou em -> app.js')
+// //console.log(nome, sobrenome);
 
-const teste = funcaoTesteIncremento(10, 2)
+// const teste = funcaoTesteIncremento(10, 2)
 
-console.log('Resultado:',teste)
+// console.log('Resultado:',teste)
+
+const _ = require('lodash')
+const chalk = require('chalk')
+
+console.log('hello')
