67e072a1b5f00b1b45666865a708412450dca678
1Nathane <nathane.miranda@gmail.com>
2021-04-21 20:52:21 -0300

    app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,74 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+const cotacoes = require('./util/cotacao')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
 
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Procure por contações',
+        author: 'Nathane Miranda'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Sobre dinamico',
+        author: 'Nathane Miranda'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Ajuda dinamico',
+        author: 'Nathane Miranda'
+    })
+})
+
 app.get('/cotacoes', (req, res) => {
-    const pessoa = {
-        nome: 'Nathane',
-        idade: 28,
-        altura: 1.59,
-        peso: 68
+
+    if(!req.query.ativo){
+        return res.status(400).json({
+            error: {
+                message: 'O ativo deve ser informado como query parameter',
+                code: 400 
+            }
+        })
     }
-    const pessoa2 = new Array()
-    pessoa2.push(pessoa)
-    pessoa2.push(pessoa)
-    res.send(pessoa2)
+
+    const symbol = req.query.ativo.toUpperCase()
+
+    cotacoes.cotacao(symbol, (err, body) => {
+        if(err){
+            res.status(err.code).json({error: {
+                message: 'O ativo deve ser informado como query parameter',
+                code: err.code
+            }})                         
+        }
+        console.log(body)
+        res.status(200).json(body)
+    })
+
+    
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        mensager: 'Página não encontrada!!!',
+        author: 'Nathane Miranda'
+    })
 })
 
 app.listen(3000, () => {
